b77526dd779f7d26cbeeb4f8b10db37a4570989d
JaeSungSim <jesung830@naver.com>
2017-10-08 17:21:56 +0900

    이미지버튼 활성화

--- JanicoProject/app/src/main/java/org/androidtown/janicoproject/MainActivity.java
+++ JanicoProject/app/src/main/java/org/androidtown/janicoproject/MainActivity.java
@@ -1,23 +1,40 @@
 package org.androidtown.janicoproject;
 
+        import android.app.Activity;
         import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.view.View.OnClickListener;
         import android.widget.Button;
+        import android.widget.ImageButton;
+        import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    ImageButton cheonggyecheon;
+    ImageView cheonggyecheon_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cheonggyecheon = (ImageButton) findViewById(R.id.cheonggyecheon);
+        cheonggyecheon.setOnClickListener(imageButtonHandler);
+        cheonggyecheon_text=(ImageView) findViewById(R.id.cheonggyecheon_text);
     }
 
-    public void menu(View view)
-    {
+    public void menu(View view) {
         Intent intent = new Intent(this, CertificationActivity.class);
         startActivity(intent);
     }
+
+    View.OnClickListener imageButtonHandler = new View.OnClickListener(){
+        public void onClick(View v) {
+            cheonggyecheon.setImageResource(R.drawable.cheonggyecheon);
+            cheonggyecheon_text.setImageResource(R.drawable.cheonggyecheon_text);
+        }
+    };
 
 }
