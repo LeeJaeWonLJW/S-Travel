53d43e12ef41160bf9fa538473a044ac8964580f
korea-1dev <31469422+korea-1dev@users.noreply.github.com>
2017-10-21 19:09:51 +0900

    modify cardview source

--- 1devProject/app/src/main/java/stravel/a1devproject/RecyclerAdapter.java
+++ 1devProject/app/src/main/java/stravel/a1devproject/RecyclerAdapter.java
@@ -1,10 +1,9 @@
 package stravel.a1devproject;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,30 +19,29 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     Context context;
     List<Recycler_item> items;
     int item_layout;
-
     public RecyclerAdapter(Context context, List<Recycler_item> items, int item_layout) {
-        this.context = context;
-        this.items = items;
-        this.item_layout = item_layout;
+        this.context=context;
+        this.items=items;
+        this.item_layout=item_layout;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_cert, null);
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_cert,null);
         return new ViewHolder(v);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final Recycler_item item = items.get(position);
-        Drawable drawable = ContextCompat.getDrawable(context, item.getImage());
+        final Recycler_item item=items.get(position);
+        Drawable drawable=context.getResources().getDrawable(item.getImage());
         holder.image.setBackground(drawable);
         holder.title.setText(item.getTitle());
         holder.cardview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, item.getTitle(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,item.getTitle(),Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -60,9 +58,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
         public ViewHolder(View itemView) {
             super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.image);
-            title = (TextView) itemView.findViewById(R.id.title);
-            cardview = (CardView) itemView.findViewById(R.id.cardview);
+            image=(ImageView)itemView.findViewById(R.id.image);
+            title=(TextView)itemView.findViewById(R.id.title);
+            cardview=(CardView)itemView.findViewById(R.id.cardview);
         }
     }
 }
